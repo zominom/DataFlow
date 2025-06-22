ee6049bab090342341cb678c47e73e3e13b93e06
Or Sapir <orsapir10@gmail.com>
2025-06-22 18:38:03 +0300

    Implemented DataFlow

---
+++ DataFlow/DataFlow/DataFlow.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class DataFlow<T> : IDataFlow<T>
+        where T : class
+    {
+        private List<IBlock<T>> _data;
+
+        void IDataFlow<T>.AddBlock(IBlock<T> block)
+        {
+            _data.Add(block);
+        }
+
+        T IDataFlow<T>.RunFlow(T input)
+        {
+            T value = input;
+            _data.ForEach(item =>
+            {
+                value = item.Process(value);
+            });
+            return value;
+        }
+
+    }
+}
--- DataFlow/DataFlow/Program.cs
+++ DataFlow/DataFlow/Program.cs
@@ -1,12 +1,5 @@
-﻿public interface IBlock<T> where T : class
-{
-    T Process(T input);
-}
+﻿
 
 
-public interface IDataFlow<T> where T : class
-{
-    T RunFlow(T input);
-    void AddBlock(IBlock<T> block);
-}
+
 
