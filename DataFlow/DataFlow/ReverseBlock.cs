894ae6d5936a9f9043ecdc64745edfd4797be590
Or Sapir <orsapir10@gmail.com>
2025-06-22 18:37:39 +0300

    Add Reverse Block

---
+++ DataFlow/DataFlow/ReverseBlock.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class ReverseBlock : IBlock<string>
+    {
+        public string Process(string input)
+        {
+            return input;
+        }
+    }
+}
