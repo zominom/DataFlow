cec422b7f9637fe2c874a8651965a058679505d1
Or Sapir <orsapir10@gmail.com>
2025-06-22 18:22:49 +0300

    Created Interfaces Files

---
+++ DataFlow/DataFlow/IDataFlow.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public interface IDataFlow<T> where T : class
+    {
+        T RunFlow(T input);
+        void AddBlock(IBlock<T> block);
+    }
+}
---
+++ DataFlow/DataFlow/UpperCaseBlock.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class UpperCaseBlock : IBlock<string>
+    {
+
+        public string Process(string input)
+        {
+            return input.ToUpper();
+        }
+    }
+}
