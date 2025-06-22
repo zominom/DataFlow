67a514b3e9e1f5b6c515bd1a5386a9732b07e081
Or Sapir <orsapir10@gmail.com>
2025-06-22 19:02:06 +0300

    Add RunFlow Program

--- DataFlow/DataFlow/Program.cs
+++ DataFlow/DataFlow/Program.cs
@@ -1,5 +1,11 @@
-﻿
+﻿using Generics;
+
+DataFlow<string> dataFlow = new DataFlow<string>();
+
+dataFlow.AddBlock(new UpperCaseBlock());
+dataFlow.AddBlock(new ReverseBlock());
+dataFlow.AddBlock(new ReplaceAWithSixBlock());
 
 
-
+dataFlow.RunFlow("MamAs6");
 
