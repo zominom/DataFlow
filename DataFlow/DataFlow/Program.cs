2e4106541e609b564bb4b87b09bc514fa75a0487
Or Sapir <orsapir10@gmail.com>
2025-06-22 18:19:29 +0300

    Created UpperCaseBlock

--- DataFlow/DataFlow/Program.cs
+++ DataFlow/DataFlow/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public interface IBlock<T> where T : class
+{
+    T Process(T input);
+}
+
+
+public interface IDataFlow<T> where T : class
+{
+    T RunFlow(T input);
+    void AddBlock(IBlock<T> block);
+}
+
