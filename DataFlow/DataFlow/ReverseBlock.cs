df842a9961784abf5d2701410327024c9b63a804
Or Sapir <orsapir10@gmail.com>
2025-06-24 21:58:35 +0300

    Implement ReverseBlock Process Function

--- DataFlow/DataFlow/ReverseBlock.cs
+++ DataFlow/DataFlow/ReverseBlock.cs
@@ -10,7 +10,7 @@ namespace Generics
     {
         public string Process(string input)
         {
-            return input;
+            return new string(input.Reverse().ToArray());
         }
     }
 }
